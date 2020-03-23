d77ff23b3ff5d74430095629ddc1283e5ec6950c
wuzheng <1345381396@qq.com>
2020-03-23 11:47:25 +0800

    开发工作
    提交时间：2020-03-23T1：46
    内容：完成个人简历录入页面（前端）以及个人简历展示页面（只是样式的设定）

---
+++ miniprogram/pages/resumeWrite/resumeWrite.js
@@ -0,0 +1,120 @@
+// pages/resumeShow/resemeShow.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array: ['男', '女'],
+    fileList: []
+
+
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+
+  //form表单提交
+  formSubmit: function(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  //form表单重置
+  formReset: function() {
+    console.log('form发生了reset事件')
+    this.setData({
+      inputValue: ""
+    })
+  },
+  /*
+  性别选择
+   */
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value,
+    })
+  },
+  /*
+  上传图片
+   */
+  afterRead(event) {
+    const {
+      file
+    } = event.detail;
+    // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+    wx.uploadFile({
+      url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
+      filePath: file.path,
+      name: 'file',
+      formData: {
+        user: 'test'
+      },
+      success(res) {
+        // 上传完成需要更新 fileList
+        const {
+          fileList = []
+        } = this.data;
+        fileList.push({ ...file,
+          url: res.data
+        });
+        this.setData({
+          fileList
+        });
+      }
+    });
+  }
+})
